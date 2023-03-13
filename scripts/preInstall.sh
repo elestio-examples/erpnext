5099047ebc900b5f2924ae714629d4a1f11b5206
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-03-13 12:05:14 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -3,5 +3,15 @@
 
 # apt install jq -y
 
-# mkdir -p ./data
-# chown -R 1000:1000 ./data
+mkdir -p ./sites
+mkdir -p ./logs
+mkdir -p ./db-data
+mkdir -p ./redis-queue-data
+mkdir -p ./redis-cache-data
+mkdir -p ./redis-socketio-data
+chown -R 1000:1000 ./sites
+chown -R 1000:1000 ./logs
+chown -R 1000:1000 ./db-data
+chown -R 1000:1000 ./redis-queue-data
+chown -R 1000:1000 ./redis-cache-data
+chown -R 1000:1000 ./redis-socketio-data
