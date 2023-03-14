33e31c515c358516d8d5574f38389e0f5036ae56
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-03-14 09:02:13 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,3 +1,17 @@
 #set env vars
 #set -o allexport; source .env; set +o allexport;
 
+mkdir -p ./assets
+mkdir -p ./db-data
+mkdir -p ./redis-queue-data
+mkdir -p ./redis-cache-data
+mkdir -p ./redis-socketio-data
+mkdir -p ./sites
+
+
+chown -R 1000:1000 ./assets
+chown -R 1000:1000 ./db-data
+chown -R 1000:1000 ./redis-queue-data
+chown -R 1000:1000 ./redis-cache-data
+chown -R 1000:1000 ./redis-socketio-data
+chown -R 1000:1000 ./sites
