f41bc21e115c4eab36b4e8d2e6fe8831a1d1f591
Prem Chavhan <pchavhan@logiquad.com>
2024-01-10 11:23:37 +0530

    Updated fixes

--- tests/build.sh
+++ tests/build.sh
@@ -7,14 +7,14 @@ sed -i '96i\
 ARG APPS_JSON_BASE64\
 RUN if [ -n "${APPS_JSON_BASE64}" ]; then \\\
     mkdir /opt/frappe && echo "${APPS_JSON_BASE64}" | base64 -d > /opt/frappe/apps.json; \\\
- fi' ./Dockerfile
+  fi' ./Dockerfile
 sed -i '106d' ./Dockerfile
 sed -i '106i\
-RUN export APP_INSTALL_ARGS="" && \\ \
-  if [ -n "${APPS_JSON_BASE64}" ]; then \\ \
-    export APP_INSTALL_ARGS="--apps_path=/opt/frappe/apps.json"; \\ \
-  fi && \\ \
-  bench init ${APP_INSTALL_ARGS} \\
+RUN export APP_INSTALL_ARGS="" && \\\
+  if [ -n "${APPS_JSON_BASE64}" ]; then \\\
+    export APP_INSTALL_ARGS="--apps_path=/opt/frappe/apps.json"; \\\
+  fi && \\\
+  bench init ${APP_INSTALL_ARGS}\\\
 ' Dockerfile
 sed -i "s~ARG APPS_JSON_BASE64~ARG APPS_JSON_BASE64=${APPS_JSON_BASE64}~g" ./Dockerfile
 rm -f compose.yaml
