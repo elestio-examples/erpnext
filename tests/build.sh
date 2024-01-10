28dd8b42a170f443c9c6e538bb594955fc4b5216
Prem Chavhan <pchavhan@logiquad.com>
2024-01-10 18:07:54 +0530

    Updated fixes

--- tests/build.sh
+++ tests/build.sh
@@ -11,11 +11,12 @@ RUN if [ -n "${APPS_JSON_BASE64}" ]; then \\\
 sed -i '106d' ./Dockerfile
 sed -i '106i\
 RUN export APP_INSTALL_ARGS="" && \\\
-  if [ -n "${APPS_JSON_BASE64}" ]; then \\\
-    export APP_INSTALL_ARGS="--apps_path=/opt/frappe/apps.json"; \\\
-  fi && \\\
-  bench init ${APP_INSTALL_ARGS}\\\
+    if [ -n "${APPS_JSON_BASE64}" ]; then \\\
+      export APP_INSTALL_ARGS="--apps_path=/opt/frappe/apps.json"; \\\
+    fi && \\\
+    bench init ${APP_INSTALL_ARGS}\\\
 ' Dockerfile
+sed -i '111d' ./Dockerfile
 sed -i "s~ARG APPS_JSON_BASE64~ARG APPS_JSON_BASE64=${APPS_JSON_BASE64}~g" ./Dockerfile
 rm -f compose.yaml
 docker buildx build . --output type=docker,name=elestio4test/erpnext:latest | docker load
--- tests/run.sh
+++ tests/run.sh
@@ -1,20 +1,3 @@
 #!/usr/bin/env bash
-# mkdir -p ./assets
-mkdir -p ./db-data
-mkdir -p ./redis-queue-data
-mkdir -p ./redis-cache-data
-mkdir -p ./redis-socketio-data
-mkdir -p ./sites
-mkdir -p ./logs
-
-
-# chown -R 1000:1000 ./assets
-chown -R 1000:1000 ./db-data
-chown -R 1000:1000 ./redis-queue-data
-chown -R 1000:1000 ./redis-cache-data
-chown -R 1000:1000 ./redis-socketio-data
-chown -R 1000:1000 ./sites
-chown -R 1000:1000 ./logs
-
 docker-compose up -d;
-sleep 150s;
+sleep 240s;
