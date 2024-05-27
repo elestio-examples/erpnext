e6aa98ac79c6cc6930ece24ab3223b2c0e030f78
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-05-27 09:33:39 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -3,20 +3,20 @@ cp images/production/Containerfile ./
 mv Containerfile Dockerfile
 APPS_JSON=./apps.json
 APPS_JSON_BASE64=$(cat ${APPS_JSON} | base64 | tr -d '\n')
-sed -i '96i\
+sed -i '99i\
 ARG APPS_JSON_BASE64\
 RUN if [ -n "${APPS_JSON_BASE64}" ]; then \\\
     mkdir /opt/frappe && echo "${APPS_JSON_BASE64}" | base64 -d > /opt/frappe/apps.json; \\\
   fi' ./Dockerfile
-sed -i '106d' ./Dockerfile
-sed -i '106i\
+sed -i '109d' ./Dockerfile
+sed -i '109i\
 RUN export APP_INSTALL_ARGS="" && \\\
     if [ -n "${APPS_JSON_BASE64}" ]; then \\\
       export APP_INSTALL_ARGS="--apps_path=/opt/frappe/apps.json"; \\\
     fi && \\\
     bench init ${APP_INSTALL_ARGS}\\\
 ' Dockerfile
-sed -i '111d' ./Dockerfile
+sed -i '114d' ./Dockerfile
 sed -i "s~ARG APPS_JSON_BASE64~ARG APPS_JSON_BASE64=${APPS_JSON_BASE64}~g" ./Dockerfile
 rm -f compose.yaml
 docker buildx build . --output type=docker,name=elestio4test/erpnext:latest | docker load
